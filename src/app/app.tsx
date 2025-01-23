18bc64efba37f1250a0dd817a69819ce361adf14
Jarrod Medrano <jmedran@gmail.com>
2025-01-23 15:34:26 -0600

    modified shadow areas

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,76 +2,79 @@ function App() {
   return (
     <main>
       <div className="flex items-center justify-center min-h-screen">
-        <div className="slideshow">
-          <div className="shadow">
-            <div className="shadow-div"></div>
-            <div className="bg"> </div>
-          </div>
-          <div className="container">
-            <div className="main-content">
-              <div className="content">
-                <p className="text-left uppercase more-from">
-                  More from Rico the Dog
-                </p>
-                <a href="https://sonyplaystation.com" target="_blank">
-                  <button
-                    type="button"
-                    className="border border-white text-white py-1 rounded-full uppercase rounded-accent"
-                  >
-                    Rico is Back!
-                  </button>
-                </a>
-                <h1 className="font-bolder text-white text-3xl mb-8 uppercase main-title">
-                  Ricobot
-                </h1>
-                <img
-                  className="cutout"
-                  src="./assets/1-foreground-cutout.png"
-                  alt="ricobot"
-                />
-                <p className="paragraph font-semibold">
-                  Charge into a brand-new supersized adventure with Rico across
-                  50 exciting and diverse worlds, available now on PS5!
-                </p>
-                <button className="bg-white text-black px-5 py-3 rounded-full font-bolder hover:bg-gray-100 uppercase cta">
-                  Learn More
-                </button>
-              </div>
+        <div className="main-thing">
+          <div className="slideshow">
+            <div className="shadow-bg">
+              <div className="shadow-div"></div>
+              <div className="bg"> </div>
+              <img
+                className="cutout"
+                src="./assets/1-foreground-cutout.png"
+                alt="ricobot"
+              />
             </div>
+            <div className="container">
+              <div className="main-content">
+                <div className="content">
+                  <p className="text-left uppercase more-from">
+                    More from Rico the Dog
+                  </p>
+                  <a href="https://sonyplaystation.com" target="_blank">
+                    <button
+                      type="button"
+                      className="border border-white text-white py-1 rounded-full uppercase rounded-accent"
+                    >
+                      Rico is Back!
+                    </button>
+                  </a>
+                  <h1 className="font-bolder text-white text-3xl mb-8 uppercase main-title">
+                    Ricobot
+                  </h1>
 
-            <div className="thumbnails">
-              <ul className="grid grid-cols-3 gap-x-3 gap-y-2">
-                <li>
-                  <button className="rounded">
-                    <img src="/assets/1-thumbnail.png" className="rounded" />
+                  <p className="paragraph font-semibold">
+                    Charge into a brand-new supersized adventure with Rico
+                    across 50 exciting and diverse worlds, available now on PS5!
+                  </p>
+                  <button className="bg-white text-black px-5 py-3 rounded-full font-bolder hover:bg-gray-100 uppercase cta">
+                    Learn More
                   </button>
-                </li>
-                <li>
-                  <button className="rounded">
-                    <img src="/assets/2-thumbnail.png" className="rounded" />
-                  </button>
-                </li>
-                <li>
-                  <button className="rounded">
-                    <img src="/assets/3-thumbnail.png" className="rounded" />
-                  </button>
-                </li>
-                <li>
-                  <button className="rounded">
-                    <img src="/assets/4-thumbnail.png" className="rounded" />
-                  </button>
-                </li>
-                <li>
-                  <button className="rounded">
-                    <img src="/assets/5-thumbnail.png" className="rounded" />
-                  </button>
-                </li>
-                <li>
-                  <button className="rounded">
-                    <img src="/assets/6-thumbnail.png" className="rounded" />
-                  </button>
-                </li>
-              </ul>
+                </div>
+              </div>
+
+              <div className="thumbnails">
+                <ul className="grid grid-cols-3 gap-x-3 gap-y-2">
+                  <li>
+                    <button className="rounded">
+                      <img src="/assets/1-thumbnail.png" className="rounded" />
+                    </button>
+                  </li>
+                  <li>
+                    <button className="rounded">
+                      <img src="/assets/2-thumbnail.png" className="rounded" />
+                    </button>
+                  </li>
+                  <li>
+                    <button className="rounded">
+                      <img src="/assets/3-thumbnail.png" className="rounded" />
+                    </button>
+                  </li>
+                  <li>
+                    <button className="rounded">
+                      <img src="/assets/4-thumbnail.png" className="rounded" />
+                    </button>
+                  </li>
+                  <li>
+                    <button className="rounded">
+                      <img src="/assets/5-thumbnail.png" className="rounded" />
+                    </button>
+                  </li>
+                  <li>
+                    <button className="rounded">
+                      <img src="/assets/6-thumbnail.png" className="rounded" />
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
