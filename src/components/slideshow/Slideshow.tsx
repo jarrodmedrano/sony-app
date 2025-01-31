99c52d480d50b6df49c344f1916d499dd308c987
Jarrod Medrano <jmedran@gmail.com>
2025-01-31 13:24:02 -0600

    fix loader size

--- src/components/slideshow/Slideshow.tsx
+++ src/components/slideshow/Slideshow.tsx
@@ -54,7 +54,7 @@ function Slideshow() {
   }, [slideData, activeId]);
 
   return isLoading ? (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center w-full h-full min-h-screen">
       <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
     </div>
   ) : (
